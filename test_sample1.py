97466301f4d9395981903eeb7de4badba0946893
Ron Aldric Ababao <ron.ababao@gmail.com>
2022-07-21 02:33:51 +0000

    tddbdd

---
+++ test_sample1.py
@@ -0,0 +1,12 @@
+
+def increment_by_one(i):
+    return i + 1
+    
+def test_answer():
+    assert increment_by_one(2) == 3
+
+def decrement_by_one(i):
+    return i - 1
+    
+def test_answer2():
+    assert decrement_by_one(3) ==2
